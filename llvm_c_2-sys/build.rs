4ca0a56fad7ac68a7ae86b37b20e68053c347a6d
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-07-12 15:45:01 +0200

    Try to use absolute paths instead of relative

--- llvm_c_2-sys/build.rs
+++ llvm_c_2-sys/build.rs
@@ -9,8 +9,14 @@ fn main() {
         .unwrap();
     let llvm_libdir = String::from_utf8(llvm_libdir.stdout).unwrap();
 
+    let cwd = std::env::current_dir().unwrap();
+
+    let llvmc2_out_dir = cwd.join("../llvm_c_2/build");
+    let llvmc2_include_dir = cwd.join("../llvm_c_2/include");
+
     println!("cargo:rustc-link-search={}", llvm_libdir);
-    println!("cargo:rustc-link-search=../llvm_c_2/build");
+    println!("cargo:rustc-link-search={}", llvmc2_out_dir.display());
+
     println!("cargo:rustc-link-lib=llvm_c_2");
     println!("cargo:rustc-link-lib=c++");
     println!("cargo:rustc-link-lib=LLVM-13");
@@ -18,7 +24,7 @@ fn main() {
     println!("cargo:rerun-if-changed=src/wrapper.h");
 
     let bindings = bindgen::Builder::default()
-        .clang_args(&["-I../llvm_c_2/include"])
+        .clang_args(&[&format!("-I{}", llvmc2_include_dir.display())])
         .header("src/wrapper.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
