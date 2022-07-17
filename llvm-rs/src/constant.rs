14488e82c643696b9f5d63dffca8b8848bd11b28
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2022-07-17 21:21:12 +0200

    Add `Constant::new_null_value`

--- llvm-rs/src/constant.rs
+++ llvm-rs/src/constant.rs
@@ -7,7 +7,11 @@ pub struct Constant {
 impl_from_as_ptr!(Constant, c::llvm_ConstantRef);
 
 impl Constant {
-    pub fn new_integer_value(ty: &Type, val: u64) -> Self {
+    pub fn new_integer_value(ty: &IntegerType, val: u64) -> Self {
         unsafe { Self::from_ptr(c::llvm_Constant_getIntegerValue(ty.as_ptr(), val)) }
+    }
+
+    pub fn new_null_value(ty: &Type) -> Self {
+        unsafe { Self::from_ptr(c::llvm_Constant_getNullValue(ty.as_ptr())) }
     }
 }
